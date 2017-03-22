b13d257e3759cd43edfdb6fda7906f7a0c48907a
Heather Brunell <ht936361@dal.ca>
2017-03-22 17:35:21 -0300

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,10 +1,14 @@
+/*
+ * Megan Brock March 21 2017
+ */
+
 import java.util.ArrayList;
 public class Player {
 	private String name;
 	private ArrayList<Card> hand;
 	
 	public Player() {
-		hand = new ArrayList<Card>(8);
+		hand = new ArrayList<Card>();
 	}
 	
 	public String getName() {
@@ -17,6 +21,28 @@ public class Player {
 	
 	public void playCard() {
 		
+	}
+	
+	//get cards in hand (returns array) - creating this to display number of cards each computer player has in hand
+	//(in game manager toString method) - HB
+	public ArrayList<Card> getHand()
+	{
+		return hand;
+	}
+	
+	//add to hand (withdraw) - HB
+	public void addToHand(Card c)
+	{
+		hand.add(c);
+	}
+	
+	//checks if there is a winner - HB
+	public boolean isWinner()
+	{
+		if (hand.isEmpty())
+			return true;
+		else
+			return false;
 	}
 	
 	public String toString() {
