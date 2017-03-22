2e4244f1a45dbb38985368c9706b8627cee7b2cf
Heather Brunell <ht936361@dal.ca>
2017-03-22 17:45:54 -0300

    Add files via upload

---
+++ ComputerPlayer.java
@@ -0,0 +1,11 @@
+
+public class ComputerPlayer extends Player{
+
+	public ComputerPlayer()
+	{
+		super();
+	}
+	
+	//TODO : needs to have methods to decide which card to play,
+	//or to withdraw. Probably the last piece to code before main method.
+}
