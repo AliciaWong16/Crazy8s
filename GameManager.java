11cbffd245dd3d4cefb6b0b1d97fd11ccf4fad86
Heather Brunell <ht936361@dal.ca>
2017-03-26 13:43:41 -0300

    Add files via upload

--- GameManager.java
+++ GameManager.java
@@ -50,7 +50,8 @@ public class GameManager {
 			compPlayer[0].addToHand(deck.withdraw());
 			compPlayer[1].addToHand(deck.withdraw());
 			compPlayer[2].addToHand(deck.withdraw());
-		}	
+		}
+		deck.addToDiscard(deck.withdraw());
 	}
 	
 	//IS PLAY LEGAL : checks for same card suit, same value, OR if it's an eight. returns true if play is legal
@@ -70,28 +71,20 @@ public class GameManager {
 	//NOTE: boolean p should be always set to false when calling class from main method, unless the player chooses to play the card they just withdrew
 	public boolean play (int index, boolean p) //takes in card index (from hand - "-1" will be withdraw for now)
 	{
-		//I think this will have to change when we start writing the main method. 
-		Player thisPlayer; //should make shallow copy
-		if (turn/4 == 0)
-			thisPlayer = player;
-		else if (turn/4 ==1)
-			thisPlayer = compPlayer[0];
-		else if (turn/4 ==2) 
-			thisPlayer = compPlayer[1];
-		else
-			thisPlayer = compPlayer[2];
-		
+
 		//if playing card that was just withdrawn
-		if (p && isPlayLegal(thisPlayer.getHand().get(thisPlayer.getHand().size()-1)))
+		if (p && isPlayLegal(player.getHand().get(player.getHand().size()-1)))
 		{
-			deck.addToDiscard(thisPlayer.playCard(thisPlayer.getHand().size()-1)); //plays last card picked up
+			deck.addToDiscard(player.playCard(player.getHand().size()-1)); //plays last card picked up
 			turn++;
 			return true;
 		}		
-		// if card index does not exist in player's hand (or not withdrawing), return false
-		else if (index<-1 || index > thisPlayer.getHand().size())
+		// if card index does not exist in player's hand (or not withdrawing), return false - play not valid
+		else if (index<-1 && index > player.getHand().size())
+		{
+			System.out.println("You can't play that card!");
 			return false;
-		
+		}
 		else if(index==-1) //if player wants to withdraw
 		{
 			//if draw pile is empty, moves discard pile into draw pile(except card 0 of discard)
@@ -99,12 +92,12 @@ public class GameManager {
 			{
 				deck.moveDiscard();
 			}
-			thisPlayer.addToHand(deck.withdraw()); 
+			player.addToHand(deck.withdraw()); 
 			return true;
 		}
-		else if (isPlayLegal(thisPlayer.getHand().get(index))) //if playing a card
+		else if (isPlayLegal(player.getHand().get(index))) //if playing a card
 		{
-			deck.addToDiscard(thisPlayer.playCard(index));//take card from player, give to discard(add to top of discard);
+			deck.addToDiscard(player.playCard(index));//take card from player, give to discard(add to top of discard);
 			turn++; //goes to next player's turn. This could be updated in main method loop instead (with a method to incrementTurn)
 			return true;
 		}
