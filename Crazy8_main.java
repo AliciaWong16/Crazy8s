e9c637ccfb159f9c8b1923926c9726d5b643bae3
AliciaWong16 <alicia.wong@yahoo.com>
2017-04-02 16:19:57 -0300

    Add files via upload

--- Crazy8_main.java
+++ Crazy8_main.java
@@ -30,7 +30,7 @@ public class Crazy8_main {
 			crazy.setOSuit(crazy.getDeck().getTopDiscard().getSuit());
 			if (turn==1)//if it's player's turn
 			{
-				System.out.println("Top of discard pile: " + crazy.getDeck().getTopDiscard());
+				System.out.println("\nTop of discard pile: " + crazy.getDeck().getTopDiscard());
 				System.out.println("\nYour cards: " + crazy.getPlayer().getHand().toString());
 				
 				//withdraws twos
@@ -62,34 +62,38 @@ public class Crazy8_main {
 					index = Integer.parseInt(input);
 				}
 				else{
-					boolean numbers = true;
-					for(int i =0; i<input.length(); i++){
-						char a = input.charAt(i);
-						if(!Character.isDigit(a)){
-							numbers = false;
-						}
-					}
-					while(!numbers){
-						System.out.print("Please enter a valid input (index of card or -1 to withdraw): ");
-						input = kb.next();
-						if(input.equals("-1")){
-							numbers = true;
-						}
-						else{
-							numbers = true;
-							for(int i =0; i<input.length(); i++){
-								char a = input.charAt(i);
-								if(!Character.isDigit(a)){
-									numbers = false;
-								}
+					boolean numbers = false;
+					boolean indexCorr = false;
+					while(!numbers || !indexCorr){
+						int count =0;
+						for(int i =0; i<input.length(); i++){
+							char a = input.charAt(i);
+							if(!Character.isDigit(a)){
+								count++;
 							}
 						}
+						if(count==0){
+							numbers = true;
+						}
+						if(numbers){
+							if(Integer.parseInt(input)>=crazy.getPlayer().getHand().size()){
+								indexCorr = false;
+							}
+							else{
+								indexCorr = true;
+							}
+						}
+						if(!numbers || !indexCorr){
+							System.out.print("Invalid input. Pick a card to play(index of card or -1 to withdraw): ");
+							input = kb.next();
+							numbers = false;
+							indexCorr=false;
+						}
 					}
-					if(numbers){
+					if(numbers && indexCorr){
 						index = Integer.parseInt(input);
 					}
-			}
-				
+				}
 				//crazy.play(index, false, crazy.getPlayer());
 				if(index != -2){
 					if(index!=-1) //playing a card
@@ -115,17 +119,11 @@ public class Crazy8_main {
 							System.out.println("\nYour cards: " + crazy.getPlayer().getHand().toString());
 							System.out.print("Do you want to play the card you just withdrew? (Y/N) ");
 							String playW = kb.next();
-							if(!playW.equalsIgnoreCase("N") && !playW.equalsIgnoreCase("Y")){
-								boolean incorrect = true;
-								while(incorrect){
-									System.out.print("Incorrect input. Please enter \"Y\" or \"N\": ");
-									playW = kb.next();
-									if(playW.equalsIgnoreCase("N") || playW.equalsIgnoreCase("Y")){
-										incorrect = false;
-									}
-								}
+							while(!playW.equalsIgnoreCase("N") && !playW.equalsIgnoreCase("Y")){								
+                                System.out.print("Incorrect input. Please enter \"Y\" or \"N\": ");
+                                playW = kb.next();								
 							}
-							else if (playW.equalsIgnoreCase("Y"))
+							if (playW.equalsIgnoreCase("Y"))
 							{
 								crazy.play(-1, true, crazy.getPlayer()); //would this still be -1? I think think the int is used this time. 
 								firstPlay=false;
@@ -134,8 +132,8 @@ public class Crazy8_main {
 					}
 				}
 				//prints player cards and top display card
-				System.out.println("Top of discard pile: " + crazy.getDeck().getTopDiscard());
-				System.out.println("\nYour cards: " + crazy.getPlayer().getHand().toString());
+				System.out.println("Your cards: " + crazy.getPlayer().getHand().toString());
+				System.out.println("\nTop of discard pile: " + crazy.getDeck().getTopDiscard());
 				
 				if(!firstPlay) //ensures top card was played by a player before applying special effects
 				{
@@ -143,16 +141,24 @@ public class Crazy8_main {
 					if(!eight){
 						if (crazy.getDeck().getTopDiscard().getValue().equals("8"))
 						{
-							System.out.println("Which suit do you want to change it to?");
-							crazy.setOSuit(kb.next());
+							System.out.print("Which suit do you want to change it to? (S or C or D or H) ");
+							String suit = kb.next();
+							while(!(suit.equalsIgnoreCase("H")||suit.equalsIgnoreCase("C")||suit.equalsIgnoreCase("D")||suit.equalsIgnoreCase("S"))){
+								System.out.println("Error. please input a valid suit letter: ");
+								suit = kb.next();
+							}
+							crazy.setOSuit(suit);
+							System.out.println("\nThe new suit is " + crazy.getOSuit());
 							eight = true;
 						}
 					}
 					else
 						crazy.setOSuit(crazy.getDeck().getTopDiscard().getSuit());
 					//test if jack
-					if (crazy.getDeck().getTopDiscard().getValue().equals("J"))
+					if (crazy.getDeck().getTopDiscard().getValue().equals("J")){
+						System.out.println("\nCP2 has lost their turn!");
 						turn=3;
+					}
 					else
 						turn=2;
 					//two's update
@@ -195,14 +201,14 @@ public class Crazy8_main {
 				}
 				
 				
-				System.out.println("\ncp1 cards"+crazy.getCompPlayer(0).getHand()); //TESTING - shows computer's hand
+				//System.out.println("\ncp1 cards"+crazy.getCompPlayer(0).getHand()); //TESTING - shows computer's hand
 				int chosen = crazy.compDecide(crazy.getCompPlayer(0),crazy.getCompPlayer(1));
-				System.out.println("index testing: " + chosen);
+				//System.out.println("index testing: " + chosen);
 			//	crazy.play(chosen,false,crazy.getCompPlayer(0));
 				if (chosen==-1) //if player withdrawing	
 				{
 					crazy.getCompPlayer(0).addToHand(crazy.getDeck().withdraw()); //-1 for cp not working?
-					System.out.println("CP0 withdrew a " + crazy.getCompPlayer(0).getHand().get(crazy.getCompPlayer(0).getHand().size()-1));
+					//System.out.println("CP0 withdrew a " + crazy.getCompPlayer(0).getHand().get(crazy.getCompPlayer(0).getHand().size()-1));
 					if (crazy.isPlayLegal(crazy.getCompPlayer(0).getHand().get((crazy.getCompPlayer(0).getHand().size()-1))))// checks if withdrawn card can be played
 					{
 						crazy.play(-1, true,crazy.getCompPlayer(0)); //would this still be -1? I think think the int is used this time.
@@ -222,6 +228,7 @@ public class Crazy8_main {
 						if (crazy.getDeck().getTopDiscard().getValue().equals("8"))
 						{
 							crazy.setOSuit(crazy.pickSuit(crazy.getCompPlayer(0)));
+							System.out.println("\nThe new suit is " + crazy.getOSuit());
 							eight = true;
 						}
 					}
@@ -229,8 +236,10 @@ public class Crazy8_main {
 						crazy.setOSuit(crazy.getDeck().getTopDiscard().getSuit());
 							
 					//test if jack
-					if (crazy.getDeck().getTopDiscard().getValue().equals("J"))
+					if (crazy.getDeck().getTopDiscard().getValue().equals("J")){
+						System.out.println("\nCP3 has lost their turn!");
 						turn=4;
+					}
 					else
 						turn=3;
 					//two's update
@@ -241,10 +250,10 @@ public class Crazy8_main {
 				}	 			
 				if(chosen != -1)
 				{
-					System.out.println("CP1 has played a " + crazy.getDeck().getTopDiscard() + " card.");
+					System.out.println("\nCP1 has played a " + crazy.getDeck().getTopDiscard() + " card.");
 				}
-				System.out.println("\nCP1 has " + crazy.getCompPlayer(0).getHand().size() + " cards left.");
-				System.out.println("Top of discard pile: " + crazy.getDeck().getTopDiscard());
+				System.out.println("CP1 has " + crazy.getCompPlayer(0).getHand().size() + " cards left.");
+				System.out.println("\nTop of discard pile: " + crazy.getDeck().getTopDiscard());
 			//	System.out.println("\ncp1 cards"+crazy.getCompPlayer(0).getHand());
 			//	System.out.println("done");
 				 	
@@ -258,7 +267,7 @@ public class Crazy8_main {
 			}
 			else if (turn ==3 && !winner) //second computer player
 			{
-				System.out.println("\ncp2 cards"+crazy.getCompPlayer(1).getHand()); //TESTING - shows computer's hand
+				//System.out.println("\ncp2 cards"+crazy.getCompPlayer(1).getHand()); //TESTING - shows computer's hand
 				//withdraws twos
 				if (two>=1 && !firstPlay)
 				{
@@ -280,14 +289,14 @@ public class Crazy8_main {
 					crazy.play(-1, false, crazy.getCompPlayer(1));
 					crazy.play(-1, false, crazy.getCompPlayer(1));
 				}
-				System.out.println("\ncp2 cards"+crazy.getCompPlayer(1).getHand()); //TESTING - shows computer's hand
+				//System.out.println("\ncp2 cards"+crazy.getCompPlayer(1).getHand()); //TESTING - shows computer's hand
 				int chosen = crazy.compDecide(crazy.getCompPlayer(1),crazy.getCompPlayer(2));
-				System.out.println("index testing: " + chosen);
+				//System.out.println("index testing: " + chosen);
 			//	crazy.play(chosen,false,crazy.getCompPlayer(0));
 				if (chosen==-1) //if player withdrawing	
 				{
 					crazy.getCompPlayer(1).addToHand(crazy.getDeck().withdraw()); //-1 for cp not working?
-					System.out.println("CP2 withdrew a " + crazy.getCompPlayer(1).getHand().get(crazy.getCompPlayer(1).getHand().size()-1));
+					//System.out.println("\nCP2 withdrew a " + crazy.getCompPlayer(1).getHand().get(crazy.getCompPlayer(1).getHand().size()-1));
 					if (crazy.isPlayLegal(crazy.getCompPlayer(1).getHand().get((crazy.getCompPlayer(1).getHand().size()-1))))// checks if withdrawn card can be played
 					{
 						crazy.play(-1, true,crazy.getCompPlayer(1)); //would this still be -1? I think think the int is used this time.
@@ -297,7 +306,7 @@ public class Crazy8_main {
 				else
 				{
 					crazy.play(chosen,false,crazy.getCompPlayer(1));
-					System.out.println("CP2 has played a " + crazy.getDeck().getTopDiscard() + " card.");
+					//System.out.println("CP2 has played a " + crazy.getDeck().getTopDiscard() + " card.");
 					firstPlay=false;
 				}
 				if (!firstPlay) //checks for special cards as long as a player has placed a card
@@ -307,6 +316,7 @@ public class Crazy8_main {
 						if (crazy.getDeck().getTopDiscard().getValue().equals("8"))
 						{
 							crazy.setOSuit(crazy.pickSuit(crazy.getCompPlayer(1)));
+							System.out.println("\nThe new suit is " + crazy.getOSuit());
 							eight = true;
 						}
 					}
@@ -326,10 +336,10 @@ public class Crazy8_main {
 				}	 			
 				if(chosen != -1)
 				{
-					System.out.println("CP2 has played a " + crazy.getDeck().getTopDiscard() + " card.");
+					System.out.println("\nCP2 has played a " + crazy.getDeck().getTopDiscard() + " card.");
 				}
-				System.out.println("\nCP2 has " + crazy.getCompPlayer(1).getHand().size() + " cards left.");
-				System.out.println("Top of discard pile: " + crazy.getDeck().getTopDiscard()); 	
+				System.out.println("CP2 has " + crazy.getCompPlayer(1).getHand().size() + " cards left.");
+				System.out.println("\nTop of discard pile: " + crazy.getDeck().getTopDiscard()); 	
 				//checks if player won
 				if (crazy.getCompPlayer(1).isWinner())
 				{
@@ -340,7 +350,7 @@ public class Crazy8_main {
 			}
 			else if (turn ==4 && !winner) //third computer player
 			{
-				System.out.println("\ncp3 cards"+crazy.getCompPlayer(2).getHand()); //TESTING - shows computer's hand
+				//System.out.println("\ncp3 cards"+crazy.getCompPlayer(2).getHand()); //TESTING - shows computer's hand
 				//withdraws twos
 				if (two>=1 && !firstPlay)
 				{
@@ -362,14 +372,14 @@ public class Crazy8_main {
 					crazy.play(-1, false, crazy.getCompPlayer(2));
 					crazy.play(-1, false, crazy.getCompPlayer(2));
 				}
-				System.out.println("\ncp3 cards"+crazy.getCompPlayer(2).getHand()); //TESTING - shows computer's hand
+				//System.out.println("\ncp3 cards"+crazy.getCompPlayer(2).getHand()); //TESTING - shows computer's hand
 				int chosen = crazy.compDecide(crazy.getCompPlayer(2),crazy.getPlayer());
-				System.out.println("index testing: " + chosen);
+				//System.out.println("index testing: " + chosen);
 			//	crazy.play(chosen,false,crazy.getCompPlayer(0));
 				if (chosen==-1) //if player withdrawing	
 				{
 					crazy.getCompPlayer(2).addToHand(crazy.getDeck().withdraw()); //-1 for cp not working?
-					System.out.println("CP3 withdrew a " + crazy.getCompPlayer(2).getHand().get(crazy.getCompPlayer(2).getHand().size()-1));
+					//System.out.println("CP3 withdrew a " + crazy.getCompPlayer(2).getHand().get(crazy.getCompPlayer(2).getHand().size()-1));
 					if (crazy.isPlayLegal(crazy.getCompPlayer(2).getHand().get((crazy.getCompPlayer(2).getHand().size()-1))))// checks if withdrawn card can be played
 					{
 						crazy.play(-1, true,crazy.getCompPlayer(2)); //would this still be -1? I think think the int is used this time.
@@ -379,7 +389,7 @@ public class Crazy8_main {
 				else
 				{
 					crazy.play(chosen,false,crazy.getCompPlayer(2));
-					System.out.println("CP3 has played a " + crazy.getDeck().getTopDiscard() + " card.");
+					//System.out.println("CP3 has played a " + crazy.getDeck().getTopDiscard() + " card.");
 					firstPlay=false;
 				}
 				if (!firstPlay) //checks for special cards as long as a player has placed a card
@@ -389,6 +399,7 @@ public class Crazy8_main {
 						if (crazy.getDeck().getTopDiscard().getValue().equals("8"))
 						{
 							crazy.setOSuit(crazy.pickSuit(crazy.getCompPlayer(2)));
+							System.out.println("\nThe new suit is " + crazy.getOSuit());
 							eight = true;
 						}
 					}
@@ -396,8 +407,10 @@ public class Crazy8_main {
 						crazy.setOSuit(crazy.getDeck().getTopDiscard().getSuit());
 							
 					//test if jack
-					if (crazy.getDeck().getTopDiscard().getValue().equals("J"))
+					if (crazy.getDeck().getTopDiscard().getValue().equals("J")){
 						turn=2;
+						System.out.println("Player has lost their turn!");
+					}
 					else
 						turn=1;
 					//two's update
@@ -408,10 +421,10 @@ public class Crazy8_main {
 				}	 			
 				if(chosen != -1)
 				{
-					System.out.println("CP3 has played a " + crazy.getDeck().getTopDiscard() + " card.");
+					System.out.println("\nCP3 has played a " + crazy.getDeck().getTopDiscard() + " card.");
 				}
-				System.out.println("\nCP3 has " + crazy.getCompPlayer(2).getHand().size() + " cards left.");
-				System.out.println("Top of discard pile: " + crazy.getDeck().getTopDiscard()); 	
+				System.out.println("CP3 has " + crazy.getCompPlayer(2).getHand().size() + " cards left.");
+				//System.out.println("Top of discard pile: " + crazy.getDeck().getTopDiscard()); 	
 				//checks if player won
 				if (crazy.getCompPlayer(2).isWinner())
 				{
@@ -419,8 +432,6 @@ public class Crazy8_main {
 					System.out.println("\n" + crazy.getPlayer().getName() + ", You Lost! CP3 Won!");
 				}
 			}
-				
-			//for computer player, inputs next player's card number as int (cardCount)
 		}	
-	}	
+	}
 }
