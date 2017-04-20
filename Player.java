410ff377d5a2506728db78cd481986275c2d0dfe
Heather Brunell <ht936361@dal.ca>
2017-04-19 23:54:25 -0300

    newmain, gm, player

--- Player.java
+++ Player.java
@@ -3,30 +3,62 @@
  */
 
 import java.util.ArrayList;
+
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Player {
+	
 	//attributes
 	private String name;
 	private ArrayList<Card> hand;
+	private ArrayList<ImageView> imageHand;
+	private ArrayList<Button> buttonHand;
 	
 	//constructors
 	public Player(String n) {
 		hand = new ArrayList<Card>();
 		name=n;
+		imageHand = new ArrayList<ImageView>();
+		buttonHand = new ArrayList<Button>();
 	}
 	public Player() {
 		hand = new ArrayList<Card>();
+		imageHand = new ArrayList<ImageView>();
+		buttonHand = new ArrayList<Button>();
 	}
 	
 	//get/set
 	public String getName() {
 		return name;
 	}
+	//get buttons (with images)
+	public ArrayList<Button> getButtons()
+	{
+	//	setImages();
+		return buttonHand;
+	}
+	//get button index - return int
+	public int getButtonIndex(Button b)
+	{
+		int r=-1;
+		for (int i=0; i<buttonHand.size(); i++)
+		{
+			if (b == buttonHand.get(i))
+				return i;
+		}
+		System.out.println("Button not found");
+		return r;
+	}
 	
 	public void setName(String n) {
 		name = n;
 	}
 	//play : takes card from player and return that card for discard pile (same as withdraw in deck class) -HB
-	public Card playCard(int index) {
+	public Card playCard(int index)
+	{
+	//	System.out.println("TEST PLAY CARD");
 			Card temp = new Card();
 			temp = getHand().get(index); //creates placeholder card(hard copy)
 			getHand().remove(index); //removes card from hand
